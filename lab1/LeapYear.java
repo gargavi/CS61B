018a9e23b55c7d42001f11524262c3a9e1bf8887
Avi Garg <avigarg2000@gmail.com>
2019-08-29 14:20:10 -0700

    I forgot to update my actual file, trying again now

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,9 +1,21 @@
 public class LeapYear {
+	int year = 2000; 
+
+	public static void main (String[] args) { 
+		if (isLeapYear(year)) {
+			System.out.println(year + "is a leap year.")
+		}
 
 
 
-
-
+	}
+	public static boolean isLeapYear(Int giv_year){ 
+		if (year % 400 == 0 ){ 
+			return true }
+		if (year % 4 == 0 && year % 100 != 0 ) { 
+			return true }
+		return false; 
+	}
 
 
  }
