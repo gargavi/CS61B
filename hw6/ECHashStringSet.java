13c76b97a300dff4cab988e6fa16dbd23cb8db67
Avi Garg <avigarg2000@gmail.com>
2019-10-31 12:45:54 -0700

    Hello

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /** A set of String values.
  *  @author
  */
 class ECHashStringSet implements StringSet {
+    private double size = 0.0;
+    private List<Node> total = new ArrayList<Node>(1);
+
+    private double load = 2.0;
+
     @Override
     public void put(String s) {
-        // FIXME
+        size = 1.0 + size;
+         
+
     }
 
     @Override
@@ -18,4 +26,17 @@ class ECHashStringSet implements StringSet {
     public List<String> asList() {
         return null; // FIXME
     }
+
+    public class Node {
+        String _string;
+        Node _next;
+        public Node(String s){
+            _string = s;
+            _next = null;
+        }
+        public void add_next(Node a){
+            _next = a;
+        }
+    }
+
 }
